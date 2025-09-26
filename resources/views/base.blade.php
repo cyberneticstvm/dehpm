f2d950bc4af95b62b37a8e74f5cf1771cab9d978
Cybernetics <mail@cybernetics.me>
2025-09-26 11:41:51 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -84,15 +84,7 @@
                                 <a href="https://devieh.com" target="_blank" class="footer-link fw-medium">Devi Eye Hospitals</a>
                             </div>
                             <div class="d-none d-lg-inline-block">
-                                <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
-                                <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More Themes</a>
-
-                                <a
-                                    href="https://demos.pixinvent.com/frest-html-admin-template/documentation/"
-                                    target="_blank"
-                                    class="footer-link me-4">Documentation</a>
-
-                                <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+                                <a href="#" class="footer-link me-4" target="_blank">Support</a>
                             </div>
                         </div>
                     </footer>

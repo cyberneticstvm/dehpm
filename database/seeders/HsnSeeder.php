a9d53287987e00ad881a8bfe43f4ecb09c2b8dd6
Cybernetics <mail@cybernetics.me>
2025-10-05 11:32:51 +0530

    small updates

--- database/seeders/HsnSeeder.php
+++ database/seeders/HsnSeeder.php
@@ -15,48 +15,56 @@ class HsnSeeder extends Seeder
     {
         Hsn::create([
             'name' => 'Lens',
+            'short_name' => 'LE',
             'code' => '90015000',
             'tax_percentage' => 5,
         ]);
 
         Hsn::create([
             'name' => 'Frame',
+            'short_name' => 'FR',
             'code' => '90031100',
             'tax_percentage' => 5,
         ]);
 
         Hsn::create([
             'name' => 'Contact Lens',
+            'short_name' => 'CL',
             'code' => '90013000',
             'tax_percentage' => 5,
         ]);
 
         Hsn::create([
             'name' => 'Sunglass',
+            'short_name' => 'SG',
             'code' => '90041000',
             'tax_percentage' => 18,
         ]);
 
         Hsn::create([
             'name' => 'Solution',
+            'short_name' => 'SO',
             'code' => '33079020',
             'tax_percentage' => 18,
         ]);
 
         Hsn::create([
             'name' => 'Accessory',
+            'short_name' => 'AC',
             'code' => '90185090',
             'tax_percentage' => 5,
         ]);
 
         Hsn::create([
             'name' => 'Ointment',
+            'short_name' => 'OI',
             'code' => '30049099',
             'tax_percentage' => 5,
         ]);
 
         Hsn::create([
             'name' => 'Eye Drop',
+            'short_name' => 'ED',
             'code' => '30042039',
             'tax_percentage' => 5,
         ]);
